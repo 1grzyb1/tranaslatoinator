976909e00c07ff00ce260865c819609af0e2d3d7
wiktor Kęska <wkeska@silenteight.com>
2021-11-01 19:22:37 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/tranaslatoinator/TranaslatoinatorApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tranaslatoinator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TranaslatoinatorApplication
+
+fun main(args: Array<String>) {
+  runApplication<TranaslatoinatorApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/tranaslatoinator/TranaslatoinatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.tranaslatoinator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TranaslatoinatorApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
