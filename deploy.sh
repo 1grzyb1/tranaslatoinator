62efbfdbe0acba2e5cbc11032e0c64bd2a8fe5a9
wiktor Kęska <wkeska@silenteight.com>
2021-11-03 23:12:29 +0100

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 gradle build
 docker build -t grzybek/translatoinator .
-ssh grzybek docker rm $(docker stop $(docker ps -a -q --filter ancestor=grzybek/translatoinator --format="{{.ID}}"))
+ssh docker stop $(docker ps -q --filter ancestor=grzybek/translatoinator )
 docker save grzybek/translatoinator | bzip2 | pv | ssh grzybek docker load
 ssh grzybek docker run -p 8080:8080 --net=host -d grzybek/translatoinator
